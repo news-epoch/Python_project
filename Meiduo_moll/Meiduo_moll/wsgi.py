99455df8602e8e7aeb1a241c4af67c4960fe1ab3
news-epoch <819730159@qq.com>
2022-01-13 09:58:24 +0800

    配置了settings，添加日志参数，修改数据库，修改缓存，添加模板，创建日志文件

--- Meiduo_moll/Meiduo_moll/wsgi.py
+++ Meiduo_moll/Meiduo_moll/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Meiduo_moll.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Meiduo_moll.settings.prod')
 
 application = get_wsgi_application()

2ec4f6992fe4d9b575e808f14f06298b27f59fff
Administrator <819730159@qq.com>
2023-12-03 20:59:40 +0800

    XGB模型GUI版本2.0--兼容版

--- reptile/Xgboost/startup.py
+++ reptile/Xgboost/startup.py
@@ -2,40 +2,35 @@ import sys, os
 import threading
 
 from PyQt5 import uic, QtGui
-from PyQt5.QtCore import QThread, Qt
+from PyQt5.QtCore import QThread, Qt, pyqtSignal
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 
+from XGboostUI import Ui_Form
 
-class MyThread(QThread):
-    def __init__(self, featuresOne, featuresTwo, featuresThree, featuresFour, featuresFive, featuresSix,message):
-        super().__init__()
+class predictThread(QThread):
+    sinOut = pyqtSignal(str)
+    def __init__(self, featuresOne, featuresTwo, featuresThree, featuresFour, featuresFive, featuresSix):
+        super(predictThread, self).__init__()
         self.featuresOne = featuresOne
         self.featuresTwo = featuresTwo
         self.featuresThree = featuresThree
         self.featuresFour = featuresFour
         self.featuresFive = featuresFive
         self.featuresSix = featuresSix
-        self.message = message
     def run(self):
-        return self.xgbThread()
-
-    def xgbThread(self):
         from xgbModel import xgbModel
         xgb = xgbModel()
         msg = xgb.predict(feature1=self.featuresOne,
-                    feature2=self.featuresTwo,
-                    feature3=self.featuresThree,
-                    feature4=self.featuresFour,
-                    feature5=self.featuresFive,
-                    feature6=self.featuresSix,
-                    )
-        # 开始渲染结果
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(18)
-        self.message.setText(msg)
+                          feature2=self.featuresTwo,
+                          feature3=self.featuresThree,
+                          feature4=self.featuresFour,
+                          feature5=self.featuresFive,
+                          feature6=self.featuresSix,
+                          )
+        self.sinOut.emit(str(msg))
+
+
+
 
 
 class Mywindow(QWidget):
@@ -46,8 +41,10 @@ class Mywindow(QWidget):
     def init_ui(self):
         self.BASE_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
         print(self.BASE_DIR)
-        self.ui = uic.loadUi(os.path.join(self.BASE_DIR, "XGboost2.0.ui"))
 
+        # self.ui = uic.loadUi(os.path.join(self.BASE_DIR, "XGboost2.0.ui"))
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
         # 提取要操作的控件
         self.featuresOne_edit = self.ui.featuresOne_edit
         self.featuresTwo_edit = self.ui.featuresTwo_edit
@@ -60,10 +57,7 @@ class Mywindow(QWidget):
 
         self.message = self.ui.message
 
-        # 绑定信号与槽函数
-        self.forecasting_button.clicked.connect(self.XGVRegressor())
 
-    def XGVRegressor(self):
         # 获取参数
         featuresOne = self.featuresOne_edit.text()
         featuresTwo = self.featuresTwo_edit.text()
@@ -72,17 +66,29 @@ class Mywindow(QWidget):
         featuresFive = self.featuresFive_edit.text()
         featuresSix = self.featuresSix_edit.text()
 
-        # 开始预测
-        predict = MyThread(featuresOne=featuresOne,
+        # 绑定信号与槽函数
+        self.predict = predictThread(featuresOne=featuresOne,
                            featuresTwo=featuresTwo,
                            featuresThree=featuresThree,
                            featuresFour=featuresFour,
                            featuresFive=featuresFive,
-                           featuresSix=featuresSix,
-                           message=self.message)
-        predict.start()
+                           featuresSix=featuresSix)
 
+        self.predict.sinOut.connect(self.XgbRegressor)
 
+        self.forecasting_button.clicked.connect(self.XgbStrart)
+
+    def XgbRegressor(self, msg):
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(18)
+        self.message.setFont(font)
+        self.message.setText(msg)
+
+    def XgbStrart(self):
+        self.predict.start()
 
 
 if __name__ == '__main__':
---
+++ reptile/Xgboost/test/test.py
@@ -0,0 +1,4 @@
+
+if __name__ == '__main__':
+    a = '1.02'
+    print(float(a))
